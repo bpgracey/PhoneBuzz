8f57159cee2e2eb8cdb99b17c607e6b21dbf725b
Bancroft Gracey <bancroft.gracey@gmail.com>
2014-06-22 20:17:06 -0700

    Write the TwiML

---
+++ PhoneBuzz/src/main/scala/com/bpgracey/phonebuzz/Answers.scala
@@ -0,0 +1,42 @@
+package com.bpgracey.phonebuzz
+
+object Answer {
+	import Config._
+	
+	def badNumber(number: Int) = {
+<Response>
+    <Say>I'm sorry, I heard {number}, which is more than {max}. Goodbye!</Say>
+</Response>
+		
+	}
+	
+	val noNumber = {
+<Response>
+    <Say>I'm sorry, I heard zero, which is less than 1. Goodbye!</Say>
+</Response>
+		
+	}
+
+	def resultAnswer(number: Int, results: Seq[String]) = {
+		val nodes = results map {r => <Say>{r}.</Say>}
+		
+<Response>
+	<Say>I heard {number}. The Fizz Buzz list from 1 to {number} is:</Say>
+	{nodes}
+	<Say>Goodbye!</Say>
+</Response>
+	}
+	
+	val firstAnswer =
+<Response>
+    <Gather action="/phonebuzz/number" method="POST" timeout="10" finishOnKey="#">
+        <Say>
+            Please enter a number between 1 and {max},
+            followed by the pound sign
+        </Say>
+    </Gather>
+    <Say>We didn't receive any input. Goodbye!</Say>
+</Response>
+	
+}
+
---
+++ PhoneBuzz/src/test/scala/com/bpgracey/phonebuzz/AnswersSpec.scala
@@ -0,0 +1,25 @@
+package com.bpgracey.phonebuzz
+
+import org.specs2.mutable.Specification
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class AnswersSpec extends Specification {
+	import Answer._
+	
+	"BadNumber" should {
+		"give the right message" in {
+			val say = badNumber(5) \ "Say"
+			say.text must contain("""I'm sorry, I heard 5, which is more than 50. Goodbye!""")
+		}
+	}
+	
+	"ResultAnswer" should {
+		"give the right message" in {
+			val result = resultAnswer(-9, Seq("A", "B"))
+			val says = (result \ "Say") map(_.text)
+			says mustEqual Seq("""I heard -9. The Fizz Buzz list from 1 to -9 is:""", """A.""", """B.""", """Goodbye!""")
+		}
+	}
+}
